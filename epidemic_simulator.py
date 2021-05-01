d22b289ac66dc05956db4fd9b9d6fd453431807d
Zsolt14 <zsoltp14@gmail.com>
2021-05-01 14:31:50 +0200

    Personal color

--- epidemic_simulator.py
+++ epidemic_simulator.py
@@ -4,17 +4,37 @@ from random import randint
 
 # Constants
 SCREEN_SIZE = WIDTH, HEIGHT = (1280, 720)
-
+NUMBER_OF_PEOPLE = 10
+PERSON_SPEED = 15
+PERSON_COLOR_CLER = (200, 150, 0)
+PERSON_COLOR_INFECTED = (200,0, 0)
+PERSON_COLOR_IMMUNE = (0, 150, 0)
 
 # Initialization
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Epidemic Simulator 1.0")
-fps = pygame-time.Clock()
+fps = pygame.time.Clock()
 pause = False
 
+# Person class
+class Person:
+
+    def __init__(self, x, y):
+          self.x = x
+          self.y = y  
+def draw(people):
+
+      screen.fill((0,0,0))
+
+      pygame.draw.circle(screen, )
+
+      pygame.display.update()
+      fps.tick(PERSON_SPEED)
+
+
 # Create people
-main_people_list = [Person(randint(50, WIDTH-50), randint(50, HEIGHT-50)) for i in range(10)]
+main_people_list = [Person(randint(50, WIDTH-50), randint(50, HEIGHT-50)) for i in range(NUMBER_OF_PEOPLE)]
 
 # Main program
 while True:
@@ -26,4 +46,4 @@ while True:
               if event.key == pygame.K_SPACE:
                     pause = not pause
   if not pause:
-      pass     
+      draw(main_people_list)   
